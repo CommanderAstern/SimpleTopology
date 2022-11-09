e00e5ec88a0efa592f4556c69492e554371fd37b
Asim Jawahir <45691479+CommanderAstern@users.noreply.github.com>
2022-11-09 22:56:38 +0530

    Update topo.py

--- topo.py
+++ topo.py
@@ -1,58 +1,52 @@
 from mininet.topo import Topo
 from mininet.net import Mininet
+from mininet.node import Node
 from mininet.log import setLogLevel, info
-from mininet.node import RemoteController
 from mininet.cli import CLI
-"""
-Instructions to run the topo:
-    1. Go to directory where this fil is.
-    2. run: sudo -E python Simple_Pkt_Topo.py.py
 
-The topo has 4 switches and 4 hosts. They are connected in a star shape.
-"""
+class LinuxRouter( Node ):
+    "A Node with IP forwarding enabled."
 
+    def config( self, **params ):
+        super( LinuxRouter, self).config( **params )
+        # Enable forwarding on the router
+        self.cmd( 'sysctl net.ipv4.ip_forward=1' )
 
-class SimplePktSwitch(Topo):
-    """Simple topology example."""
-
-    def __init__(self, **opts):
-        """Create custom topo."""
-
-        # Initialize topology
-        # It uses the constructor for the Topo cloass
-        super(SimplePktSwitch, self).__init__(**opts)
-
-        # Add hosts and switches
-        h1 = self.addHost('h1',ip='192.168.1.1')
-        h2 = self.addHost('h2',ip='192.168.1.2')
-        h3 = self.addHost('h3',ip='192.168.1.3')
-        h4 = self.addHost('h4',ip='192.168.1.4')
-        h5 = self.addHost('h4',ip='192.168.1.5')
+    def terminate( self ):
+        self.cmd( 'sysctl net.ipv4.ip_forward=0' )
+        super( LinuxRouter, self ).terminate()
 
 
-        # Adding switches
+class NetworkTopo( Topo ):
+    "A LinuxRouter connecting three IP subnets"
+
+    def build( self, **_opts ):
+
+        defaultIP = '10.0.1.1/24'  # IP address for r0-eth1
+        router = self.addNode( 'r0', cls=LinuxRouter, ip=defaultIP )
+
+        h1 = self.addHost( 'h1', ip='10.0.1.2/24', defaultRoute='via 10.0.1.1' )
+        h2 = self.addHost( 'h2', ip='10.0.2.2/24', defaultRoute='via 10.0.2.1' )
+        h3 = self.addHost( 'h3', ip='10.0.2.3/24', defaultRoute='via 10.0.2.1' )
+
         s1 = self.addSwitch('s1')
 
-        # Add links
-        self.addLink(h1, s1)
+        self.addLink( h1, router, intfName2='r0-eth1', params2={ 'ip' : '10.0.1.1/24' } )
+        self.addLink( s1, router, intfName2='r0-eth2', params2={ 'ip' : '10.0.2.1/24' } )
+
         self.addLink(h2, s1)
         self.addLink(h3, s1)
-        self.addLink(h4, s1)
-        self.addLink(h5, s1)
-
-
-
 
 def run():
-    c = RemoteController('c', '0.0.0.0', 6633)
-    net = Mininet(topo=SimplePktSwitch(), controller=None)
-    net.addController(c)
+    "Test linux router"
+    topo = NetworkTopo()
+    net = Mininet( topo=topo )  # controller is used by s1-s3
     net.start()
-
-    CLI(net)
+    info( '*** Routing Table on Router:\n' )
+    print net[ 'r0' ].cmd( 'route' )
+    CLI( net )
     net.stop()
 
-# if the script is run directly (sudo custom/optical.py):
 if __name__ == '__main__':
-    setLogLevel('info')
+    setLogLevel( 'info' )
     run()
